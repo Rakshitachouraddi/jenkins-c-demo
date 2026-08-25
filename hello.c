f18f5c728c244d42ed10f375d7f5053b8fd96432
Rakshita Chouraddi <rakshitachouraddi@gmail.com>
2026-08-25 09:43:46 +0000

    This is my modified code

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main()
 {
-printf("hello from Rakshita-Jenkins webhook test");
+printf("hello from Rakshita-Jenkins webhook test\n");
+printf("This is modified line\n");
 return 0;
 }
