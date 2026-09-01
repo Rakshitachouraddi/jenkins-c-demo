2e7b396da63af21ecf2ebce377c6308b7d616ff8
Rakshita Chouraddi <rakshitachouraddi@gmail.com>
2026-09-01 04:26:47 +0000

    Add C calculator program

---
+++ calculator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a=20,b=30;
+printf("Addition=%d\n",a+b);
+printf("substraction=%d\n",a-b);
+printf("multiplication=%d\n",a*b);
+printf("division=%d\n",a/b);
+return 0;
+}
+
